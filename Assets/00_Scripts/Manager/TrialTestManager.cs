88c4eae8789da28c645079dc576ac5b8f7991d90
Nor_Book <thanawat.srit@kmutt.ac.th>
2024-11-15 09:26:38 +0700

    FIX: TrialTestManager.cs >>> Can Set number of trial

--- Assets/00_Scripts/Manager/TrialTestManager.cs
+++ Assets/00_Scripts/Manager/TrialTestManager.cs
@@ -1,10 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
-//using System.IO.Ports; 
+//using System.IO.Ports; // Config is into Edit>ProjectSettings>Player>ApiCompatibilityLevel to .NET framework
 using UnityEngine;
 using UnityEngine.UI;
 using LSL;
-
+using TMPro;
 
 public class TrialTestManager : MonoBehaviour
 {
@@ -13,38 +13,64 @@ public class TrialTestManager : MonoBehaviour
     public Color targetColor = Color.red;
 
     [Range(1, 40)]
-    [SerializeField] List<int> frequencyImg;
+    public int frequencyDown = 1; // Frequency in Hz
 
-    //Image Blinking Paramemter '
-    [SerializeField] List<Image> blinkImg;
+    [Range(1, 40)]
+    public int frequencyLeft = 1; 
 
+    [Range(1, 40)]
+    public int frequencyUp = 1;
 
-    private float elapsedTime = 0f;
-    private bool isStartColor = true;
+    [Range(1, 40)]
+    public int frequencyRight = 1;
 
-    //For interval calculations
-    private float interval;
-    //For cue
+    public Image blinkImgDown;
+    public Image blinkImgLeft;
+    public Image blinkImgUp;
+    public Image blinkImgRight;
+
+    public TMP_Text delayText; // Text to display during the delay
+
+    public float delayBeforeExperiment = 5f;
     public float blinkDuration = 5.0f;
     public float delayBeforeBlink = 1f;
     public float delayBeforeNextTrial = 2f;
+    public int numberOfTrial = 4;
+
+    //For image blinking
+    private float elapsedTime = 0f;
+    private bool isStartColor = true;
+    private bool SSVEPTrigger = true;
+
+    //For interval calculations
+    private float intervalDown;
+    private float intervalLeft;
+    private float intervalUp;
+    private float intervalRight;
+
+    //For delay before start
+    private bool isBlinkingActive = false; // To check if blinking should start
+
+    // For calculate equally class trials
+    private int[] randomNumbers;
+    private int currentIndex = 0;
     private float indexTimer = 0f;
+
+    //For main timer control
     private float delayTimer = 0f;
     private bool isDelayActive = false;
 
-    string StreamName = "UnityEventSSVEP"; // Stream Name
+    //For LSL
+    string StreamName = "UnityEvent";
     string StreamType = "Markers";
     private StreamOutlet outlet;
     private string[] sample = { "" };
 
-    // For calculate equally class trials
-    public int numberOfTrial = 10;
-    public int numberOfClass = 2;
-    private int[] randomNumbers;
-    private int currentIndex = 0;
+    //private SerialPort serialPort;
 
-    bool startTest = false;
+    private bool startTest = false;
 
+    // Start is called before the first frame update
     void Start()
     {
         var hash = new Hash128();
@@ -55,76 +81,182 @@ public class TrialTestManager : MonoBehaviour
             channel_format_t.cf_string, hash.ToString());
         outlet = new StreamOutlet(streamInfo);
 
-        if (outlet != null)
-        {
-            sample[0] = "Experiment_Begin"; //Event Name
-            outlet.push_sample(sample);
-        }
+        //serialPort = new SerialPort("COM5", 115200);
 
+        //try
+        //{
+        //    serialPort.Open();
+        //    Debug.Log("Serial port opened successfully.");
+        //}
+        //catch (System.Exception e)
+        //{
+        //    Debug.LogError("Error opening serial port: " + e.Message);
+        //}
+
+        //// Initialize the array with the desired number of random numbers
         GenerateEqualRandomNumbers();
-        for (int i = 0; i < randomNumbers.Length; i++)
-        {
-            Debug.Log(randomNumbers[i]);
-        }
-        interval = 1f / frequencyImg[randomNumbers[currentIndex]];
+
+        intervalDown = 1f / frequencyDown; // Calculate the interval based on frequency
+        intervalLeft = 1f / frequencyLeft; 
+        intervalUp = 1f / frequencyUp;
+        intervalRight = 1f / frequencyRight;
+        
     }
 
     // Update is called once per frame
-    void Update()
-    {   
-        SetStartTestTime();
-        if(startTest){
+    void Update()  
+    {
+        
+        if(GameManager.Instance){
+            if(!startTest){
+                SetStartTestTime();
+                return;
+            }
+        }else{
+            delayText.gameObject.SetActive(true);
+            delayText.text = $"Starting in {Mathf.Ceil(delayBeforeExperiment)} seconds...";
+        }
+        
+        
+        if (!isBlinkingActive)
+        {
+            // During the delay period
+            delayBeforeExperiment -= Time.deltaTime; // Decrement the delay timer
+            delayText.text = $"Starting in {Mathf.Ceil(delayBeforeExperiment)} seconds...";
+
+            if (delayBeforeExperiment <= 0f)
+            {
+                // Start blinking after the delay
+                isBlinkingActive = true;
+                delayText.enabled = false; // Hide the delay text
+                if (outlet != null)
+                {
+                    //SendSerrialPortTrigger(1);
+                    sample[0] = "Trial_Begin";
+                    outlet.push_sample(sample);
+                }
+            }
+        }
+
+        else
+        {
+            if (currentIndex >= randomNumbers.Length)
+            {
+                // Indicate completion
+                //if (serialPort != null && serialPort.IsOpen)
+                //{
+                //    serialPort.Close();
+                //}
+
+                return;
+            }
 
             if (isDelayActive)
             {
+                // During delay before moving to next index
                 delayTimer += Time.deltaTime;
-                if (delayTimer >= delayBeforeNextTrial) // Delay for blank time 
+                if (delayTimer >= delayBeforeNextTrial)
                 {
+                    currentIndex++;
+                    // Reset the timer and move to the next index after the delay
+                    if (currentIndex < randomNumbers.Length)
+                    {
+                        //SendSerrialPortTrigger(1);
+                        sample[0] = "Trial_Begin";
+                        outlet.push_sample(sample);
+                    }
+                    else
+                    {
+                        //SendSerrialPortTrigger(40);
+                        sample[0] = "End_Experiment";
+                        outlet.push_sample(sample);
+                    }
 
                     delayTimer = 0f;
                     isDelayActive = false;
-
-                    currentIndex++;
-                    interval = 1f / frequencyImg[randomNumbers[currentIndex]];
                 }
             }
 
             else
             {
+                // Increment the index timer
                 indexTimer += Time.deltaTime;
 
-                if (indexTimer < blinkDuration + delayBeforeBlink) // Delay for Blink duration 
+                if (indexTimer < blinkDuration + delayBeforeBlink)
                 {
-                    if (indexTimer < delayBeforeBlink) // Delay for cue duration 
+                    int currentRandomNumber = randomNumbers[currentIndex];
+
+                    // Check for the current random number
+                    if (currentRandomNumber == 0)
                     {
-                        blinkImg[randomNumbers[currentIndex]].color = targetColor;
+                        if (indexTimer < delayBeforeBlink)
+                        {
+                            blinkImgLeft.color = targetColor;
+                        }
+                        else
+                        {
+                            HandleBlinking(blinkImgLeft, intervalLeft, "SSVEPLeft", 2);
+                        }
+                    }
+                    else if (currentRandomNumber == 1)
+                    {
+                        if (indexTimer < delayBeforeBlink)
+                        {
+                            blinkImgRight.color = targetColor;
+                        }
+                        else
+                        {
+                            HandleBlinking(blinkImgRight, intervalRight, "SSVEPRight", 4);
+                        }
+                    }
+                    else if (currentRandomNumber == 2)
+                    {
+                        if (indexTimer < delayBeforeBlink)
+                        {
+                            blinkImgUp.color = targetColor;
+                        }
+                        else
+                        {
+                            HandleBlinking(blinkImgUp, intervalUp, "SSVEPUp", 8);
+                        }
                     }
                     else
                     {
-                        elapsedTime += Time.deltaTime; // Increment elapsed time
-
-                        if (elapsedTime >= interval / 2f) // Check if it's time to switch colors
+                        if (indexTimer < delayBeforeBlink)
                         {
-                            blinkImg[randomNumbers[currentIndex]].color = isStartColor ? endColor : startColor;// Color changing
-                            isStartColor = !isStartColor; // Toggle the color flag
-                            elapsedTime = 0f; // Reset the elapsed time
+                            blinkImgDown.color = targetColor;
+                        }
+                        else
+                        {
+                            HandleBlinking(blinkImgDown, intervalDown, "SSVEPDown", 10);
                         }
                     }
                 }
 
                 else
                 {
+                    // Reset the timer and move to the next index after the duration
+                    //SendSerrialPortTrigger(20);
+                    sample[0] = "End_of_trial";
+                    outlet.push_sample(sample);
                     indexTimer = 0f;
-                    blinkImg[randomNumbers[currentIndex]].color = startColor;
+                    blinkImgLeft.color = startColor;
+                    blinkImgRight.color = startColor;
+                    blinkImgUp.color = startColor;
+                    blinkImgDown.color = startColor;
                     isDelayActive = true;
+                    SSVEPTrigger = true;
                 }
             }
         }
+
     }
 
-    void SetStartTestTime(){
+    private void SetStartTestTime(){
         if(GameManager.Instance.IsPlayerTurn && !startTest){
             startTest = true;
+            delayText.gameObject.SetActive(true);
+            delayText.text = $"Starting in {Mathf.Ceil(delayBeforeExperiment)} seconds...";
         }
     }
 
@@ -133,12 +265,12 @@ public class TrialTestManager : MonoBehaviour
         randomNumbers = new int[numberOfTrial];
 
         // Calculate the number of times each number should appear
-        int countPerNumber = numberOfTrial / numberOfClass;
-        int remainder = numberOfTrial % numberOfClass;
+        int countPerNumber = numberOfTrial / 4;
+        int remainder = numberOfTrial % 4;
 
         // Fill the array with equal numbers of 0, 1, 2, and 3
         int index = 0;
-        for (int num = 0; num < numberOfClass; num++)
+        for (int num = 0; num < 4; num++)
         {
             for (int i = 0; i < countPerNumber; i++)
             {
@@ -149,8 +281,9 @@ public class TrialTestManager : MonoBehaviour
         // Distribute the remainder randomly
         for (int i = 0; i < remainder; i++)
         {
-            randomNumbers[index++] = Random.Range(0, numberOfClass);
+            randomNumbers[index++] = Random.Range(0, 4);
         }
+
         // Shuffle the array to randomize the order
         ShuffleArray(randomNumbers);
     }
@@ -166,4 +299,36 @@ public class TrialTestManager : MonoBehaviour
         }
     }
 
+    private void HandleBlinking(Image Img_direct, float interval, string direction, int nowCond)
+    {
+        if (SSVEPTrigger)
+        {
+            //SendSerrialPortTrigger(nowCond);
+            sample[0] = direction;
+            outlet.push_sample(sample);
+        }
+
+        elapsedTime += Time.deltaTime; // Increment elapsed time
+
+
+        if (elapsedTime >= interval / 2f) // Check if it's time to switch colors
+        {
+            //Debug.Log(elapsedTime);
+            //Debug.Log(interval / 2f);
+            Img_direct.color = isStartColor ? endColor : startColor;// Toggle the color
+            isStartColor = !isStartColor; // Toggle the color flag
+            elapsedTime = 0f; // Reset the elapsed time
+        }
+
+        SSVEPTrigger = false;
+    }
+
+    //private void SendSerrialPortTrigger(int nowCond)
+    //{
+    //    if (serialPort.IsOpen)
+    //    {
+    //        byte[] triggerBytes = new byte[] { (byte)nowCond };
+    //        serialPort.Write(triggerBytes, 0, 1);
+    //    }
+    //}
 }
